ceec5e4b36a488da0359763ea90a4c5b7b8d6411
felixchaotw <felixchaotw@gmail.com>
2023-01-17 21:45:02 +0800

    working on...

--- 2017/DT.v
+++ 2017/DT.v
@@ -13,20 +13,25 @@ module DT(
 	);
 
 
-reg [2:0] state;
-reg [2:0] NextState;
+reg [3:0] state;
+reg [3:0] NextState;
 reg [3:0] count;
+reg [7:0] temp;
+reg object;
 
 
 
 
-parameter Initialize = 0;
-parameter Read_Forward = 1;
-parameter Write_Forward = 2;
-parameter Read_Backward = 3;
-parameter Write_Backward = 4;
-parameter Done = 5;
-
+parameter Initialize_Read = 0;
+parameter Initialize_Write = 1;
+parameter Initialize_Done = 2;
+parameter Forward_Read = 3;
+parameter Forward_Write = 4;
+parameter Forward_Done = 5;
+parameter Backward_Read = 6;
+parameter Backward_Write = 7;
+parameter Backward_Done = 8;
+parameter Finish = 9;
 
 
 //Sequential Circuit
@@ -34,14 +39,16 @@ always@(posedge clk or negedge reset)
 begin
 	if(!reset)
 	begin
-		state <= Initialize;
-		count <= 0;
+		state <= Initialize_Read;
+		count <= 15;
 		done <= 0;
 		sti_rd <= 0;
 		sti_addr <= 0;
 		res_rd <= 0;
 		res_wr <= 0;
-		res_addr <= 0;
+		res_addr <= 16383;
+		temp <= 0;
+		object <= 0;
 	end
 	else 
 	begin
@@ -49,53 +56,163 @@ begin
 
          case(state)
 
-		 Initialize: begin
-                          if(sti_addr == 1023)
+		 Initialize_Read: begin
+                              sti_rd <= 1;
+						  end
+
+		 
+
+		 Initialize_Write: begin
+			                   if(res_addr == 16382)
+							   begin
+								   res_addr <= res_addr + 1;
+                                   res_do <= sti_di[count];
+						           sti_rd <= 0;
+                                   count <= 0;
+							   end
+			                   else if(count == 0)
+							   begin
+								   res_do <= sti_di[count];
+                                   res_addr <= res_addr + 1;
+								   sti_addr <= sti_addr + 1;
+                                   count <= 15;
+							   end
+							   else
+							   begin
+                                    res_wr <= 1;
+									res_do <= sti_di[count];
+                                    res_addr <= res_addr + 1;
+									count <= count - 1;
+							   end
+		                   end
+
+		Initialize_Done:begin
+                           res_wr <= 0;
+						   res_rd <= 1;
+						   res_addr <= 0;
+		                end	
+
+		Forward_Read:begin
+                         if(res_di == 1)
+						    object <= 1;
+						 else
+						    object <= 0;
+		             end
+
+        Forward_Write:begin
+						if(object == 1)
+						begin
+                            case(count)
+							0:begin
+							     res_addr <= res_addr - 129;
+								 count <= 1;
+							  end
+
+							1: begin
+                                  temp <= res_di;
+								  res_addr <= res_addr + 1;
+								  count <= 2;
+							   end
+							2: begin
+                                  temp <= (res_di < temp ? res_di : temp);
+								  res_addr <= res_addr + 1;
+								  count <= 3;
+						       end
+
+							3: begin
+                                  temp <= (res_di < temp ? res_di : temp);
+								  res_addr <= res_addr + 126;
+								  count <= 4;
+							   end
+
+							4: begin
+                                  temp <= (res_di < temp? res_di : temp);
+								  res_addr <= res_addr + 1;
+								  count <= 5;
+							   end
+
+							5: begin
+								  res_wr <= 1;
+                                  res_do <= temp + 1;
+								  count <= 0;
+							   end
+							endcase
+						end
+		              end
+
+		 Forward_Done: begin
+			              if(res_addr == 16383)
 						  begin
-                            
+							 res_wr <= 0;
+                             res_addr <= 16383;
 						  end
 						  else
 						  begin
-							sti_rd <= 1;
-							sti_addr <= sti_addr + 1;
+			                res_wr <= 0;
+						    res_rd <= 1;
+			                res_addr <= res_addr + 1;
 						  end
-
-		             end
-
-		 Forward_Pass: begin
-			                if(count == 16384)
-							begin
-								sti_rd <= 0;
-							end
-							else
-							begin
-								sti_rd <= 1;
-							    sti_addr <= count;
-							    count <= count + 1;
-							    i <= 0;
-							end
 		               end
 
-	    Window_Calculate: begin
-                              if(i == 15)
-							  begin
-                                   
-							  end
-							  else
-							  begin
-								  if(sti_di[i] == 1)
-								  begin
-									 
-								  end
-								  last_pixel <= 
-								  i <= i+1;
-							  end
-		                  end
+		 Backward_Read: begin
+                          if(res_di > 0)
+						      object <= 1;
+						  else
+						      object <= 0;
+		                end
 
-		Write_Forward:begin
-                           
-		              end			
-		 endcase
+		 Backward_Write: begin
+						if(object == 1)
+						begin
+                            case(count)
+							0:begin
+								 temp <= res_di;
+							     res_addr <= res_addr + 129;
+								 count <= 1;
+							  end
+
+							1: begin
+                                  temp <= ((res_di+1) < temp ? (res_di+1) : temp);
+								  res_addr <= res_addr - 1;
+								  count <= 2;
+							   end
+							2: begin
+                                  temp <= ((res_di+1) < temp ? (res_di+1) : temp);
+								  res_addr <= res_addr - 1;
+								  count <= 3;
+						       end
+
+							3: begin
+                                  temp <= ((res_di+1) < temp ? (res_di+1) : temp);
+								  res_addr <= res_addr - 126;
+								  count <= 4;
+							   end
+
+							4: begin
+                                  temp <= ((res_di+1) < temp ? (res_di+1) : temp);
+								  res_addr <= res_addr - 1;
+								  count <= 5;
+							   end
+
+							5: begin
+								  res_wr <= 1;
+                                  res_do <= temp;
+								  count <= 0;
+							   end
+							endcase
+						end
+		                end
+
+		 Backward_Done: begin
+			                res_wr <= 0;
+						    res_rd <= 1;
+			                res_addr <= res_addr - 1;
+		                end
+
+		Finish: begin
+                     done <= 1;
+		        end
+	     endcase
 	end
 end
 
@@ -105,24 +222,65 @@ end
 always@(*)
 begin
 	case(state)
-	Initialize:begin
-		             
-	           end
-    Forward_Pass:begin
-                    NextState = Window_Calculate;
+	Initialize_Read:begin
+		              NextState = Initialize_Write;
+	                end
+
+	Initialize_Write:begin
+                       if(res_addr == 16382)
+					      NextState = Initialize_Done;
+					   else
+					      NextState = Initialize_Write;
+	                 end
+
+	Initialize_Done:begin
+                         NextState = Forward_Read;
+	                end
+
+    Forward_Read:begin
+		             NextState = Forward_Write;
+				 end
+
+    Forward_Write:begin
+                      if(count == 5 || object == 0)
+                        NextState = Forward_Done;
+					  else
+					    NextState = Forward_Write;
+	              end
+
+    Forward_Done:begin
+		             if(res_addr == 16383)
+					   NextState = Backward_Read;
+					 else
+		               NextState = Forward_Read;
 	             end
 
-    Window_Calculate: begin
-                           NextState = Write_Forward;
-		              end				 
-
-    Write_Forward:begin
-                         NextState = Forward_Pass;
+    Backward_Read: begin
+                       NextState = Backward_Write;
 	               end
 
-    Backward_pass:begin
-		              NextState = Backward_pass;
-	              end
+	Backward_Write: begin
+                        if(count == 5 || object == 0)
+						  NextState = Backward_Done;
+						else
+						  NextState = Backward_Write;
+	                end
+
+	Backward_Done: begin
+                       if(res_addr == 0)
+					     NextState = Finish;
+					   else
+					     NextState = Backward_Read;
+	               end
+
+	Finish:begin
+		       NextState = Finish;
+	       end
+
+	default: begin
+                  NextState = NextState;
+	         end
+
 	endcase
 end
 
--- 2017/testfixture.v
+++ 2017/testfixture.v
@@ -1,5 +1,5 @@
 `timescale 1ns/10ps
-`define CYCLE      20.0          	  // Modify your clock period here
+`define CYCLE      10.0          	  // Modify your clock period here
 `define SDFFILE    "../SYN/DT_syn.sdf"	  // Modify your sdf file name
 `define End_CYCLE  100000000             // Modify cycle times once your design need more cycle times!
 
